dd8fc8d5d2172a985d1546bd683374f8f1033a18
tsvalencio-IA <tsvalencio@gmail.com>
2025-12-17 21:28:47 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'thiago-kyb-v1.0';
+const urlsToCache = [
+    './',
+    './index.html',
+    './apresentacao.html',
+    './assets/targets.mind',
+    './assets/mascote.mp4',
+    './assets/mascote_perfil.jpg', // Adicione essa foto na pasta assets
+    'https://cdn.tailwindcss.com',
+    'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap',
+    'https://cdn.jsdelivr.net/npm/boxicons@2.1.4/css/boxicons.min.css',
+    'https://aframe.io/releases/1.4.2/aframe.min.js',
+    'https://cdn.jsdelivr.net/npm/mind-ar@1.2.2/dist/mindar-image-aframe.prod.js'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting(); 
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))));
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+});
